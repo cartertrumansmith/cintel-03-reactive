79629ee34bd2fc84985c07055749fd19c6a660d1
Carter Smith <89223971+cartertrumansmith@users.noreply.github.com>
2024-11-10 19:53:11 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+import plotly.express as px
+from shiny.express import input, ui, render
+from shinywidgets import render_plotly
+import seaborn as sns
+import palmerpenguins
+
+ui.page_opts(title="Filling layout", fillable=True)
+
+penguins_df = palmerpenguins.load_penguins()
+# Add a Shiny UI sidebar for user interaction
+with ui.sidebar(open="open"):
+    # Use the ui.h2() function to add a 2nd level header to the sidebar
+    ui.h2("Sidebar")
+    
+    # Use ui.input_selectize() to create a dropdown input to choose a column
+    ui.input_selectize("selected_attribute","Selected Attribute",["choice 1","choice 2","choise 3"])
+    
+    # Use ui.input_numeric() to create a numeric input for the number of Plotly histogram bins
+    ui.input_numeric("plotly_bin_count","Plotly Bin Count",1,min=1)
+    
+    # Use ui.input_slider() to create a slider input for the number of Seaborn bins
+    ui.input_slider("seaborn_bin_count","Seaborn Bin Count",value=1,min=1,max=100)
+
+    # Use ui.input_checkbox_group() to create a checkbox group input to filter the species
+    ui.input_checkbox_group("selected_species_list","Selected Species List",["Adelie", "Gentoo", "Chinstrap"],selected=["Adelie"],inline=True)
+
+    # Use ui.hr() to add a horizontal rule to the sidebar
+    ui.hr()
+
+    # Use ui.a() to add a hyperlink to the sidebar
+    ui.a("Github",href="https://github.com/cartertrumansmith/cintel-02-data",target="_blank")
+
+with ui.layout_columns():
+    with ui.card(full_screen=True):
+        ui.card_header("Data Table")
+
+        @render.data_frame
+        def species_data():
+            return penguins_df[penguins_df['species'].isin(input.selected_species_list())]
+
+        with ui.card(full_screen=True):
+            ui.card_header("Data Grid")
+            @render.data_frame
+            def grid():
+               return render.DataGrid(data=penguins_df)
+
+
+
+with ui.layout_columns():
+    with ui.card(full_screen=True):
+        ui.card_header("Plotly Histogram: Species")
+
+        @render_plotly
+        def histogram_plotly():
+            return px.histogram(penguins_df, x="bill_length_mm",nbins=input.plotly_bin_count())
+    with ui.card(full_screen=True):
+        ui.card_header("Plotly Histogram: Species")
+
+        @render.plot
+        def histogram_seaborn():
+            return sns.histplot(data=penguins_df,x="bill_length_mm",bins=input.seaborn_bin_count())
+    with ui.card(full_screen=True):
+        ui.card_header("Plotly Scatterplot: Species")
+
+        @render_plotly
+        def plotly_scatterplot():
+            return px.scatter(data_frame=penguins_df,x="bill_length_mm", y="body_mass_g",color="species",hover_name="island",symbol="sex")
